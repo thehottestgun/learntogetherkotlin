e3be3dfa609f4100487dfaab844952bfb64b49c9
Kamil Barczak <38687751+thehottestgun@users.noreply.github.com>
2023-11-22 18:15:38 +0100

    Finished article view

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.learntogether
 
 import android.os.Bundle
-import android.preference.PreferenceActivity.Header
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Article("Android")
+                    Article()
                 }
             }
         }
@@ -33,12 +38,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Article(name: String, modifier: Modifier = Modifier) {
-    HeaderImage()
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Article(modifier: Modifier = Modifier) {
+    Column {
+        HeaderImage(modifier)
+        ArticleTitle(modifier)
+        ArticleIntro(modifier)
+        ArticleBody(modifier)
+    }
 }
 
 @Composable
@@ -48,19 +54,36 @@ fun HeaderImage(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ArticleIntro(modifier: Modifier = Modifier){
+fun ArticleTitle(modifier: Modifier = Modifier){
+    Text(
+        text = stringResource(id = R.string.article_name),
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp)
+    )
+}
 
+@Composable
+fun ArticleIntro(modifier: Modifier = Modifier){
+    Text(
+        text = stringResource(id = R.string.article_intro),
+        modifier = Modifier.padding(horizontal = 16.dp),
+        textAlign = TextAlign.Justify
+    )
 }
 
 @Composable
 fun ArticleBody(modifier: Modifier = Modifier){
-
+    Text(
+        text = stringResource(id = R.string.article_body),
+        modifier = Modifier.padding(16.dp),
+        textAlign = TextAlign.Justify
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearnTogetherTheme {
-        Article("Android")
+        Article()
     }
 }
